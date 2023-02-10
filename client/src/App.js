aad4c3d9bd0f41652d9b1e5230f2163c0e1841e5
thomasnielsen00 <you@example.com>
2023-02-10 08:52:11 +0100

    Server side running @skjalgsk

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+// import React, { useEffect, useState } from "react";
+
+function App() {
+  // const [backEndData, setBackEndData] = useState([{}]);
+
+  // Midlertidig, disse funksjonene skal gjøre i en service fil med axios
+  // useEffect(() => {
+  //   fetch("/api")
+  //     .then((response) => response.json())
+  //     .then((data) => setBackEndData(data));
+  // }, []);
+
+  return <div>Test</div>;
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/api", (request, response) => {
+  response.json({ books: ["Book1", "Book2", "Book3"] });
+});
+
+//Server running on port 5000, react app on port 3000
+const port = 5000;
+app.listen(port, () => {
+  console.info(`Server running on port ${port}`);
+});
