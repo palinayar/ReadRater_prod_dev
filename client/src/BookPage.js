2f361f89ba5742e3d25a644d76983f7c2930af54
vidarsgl <vidarsgl@stud.ntnu.no>
2023-03-09 09:43:41 +0100

    finished search functionality

--- client/src/BookPage.js
+++ client/src/BookPage.js
@@ -13,6 +13,7 @@ import {
   Typography,
   Container,
   Link,
+  TextField,
 } from "@material-ui/core";
 import Book from "./Book.js";
 import bookIconW from "./images/book-icon_white.png";
@@ -24,7 +25,6 @@ import {
   Routes,
 } from "react-router-dom";
 import readService from "./service.js";
-import { searchBooks } from "./service.js";
 import { UserContext } from "./context.js";
 
 function Copyright() {
@@ -47,6 +47,8 @@ const appBarHeight = "70px";
 export default function BookPage() {
   const [books, setBooks] = useState([]);
   const { user, setUser } = useContext(UserContext);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
   // const [rating, setRating] = useState();
   //This is how you fetch the data from the database, use in components
@@ -86,157 +88,168 @@ export default function BookPage() {
     }
   };
 
-  const SearchBooks = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [books, setBooks] = useState([]);
-
-    const handleSearch = async () => {
-      const results = await searchBooks(searchTerm);
-      setBooks(results);
+  const filterSearchedBooks = books.filter((book) => {
+    if (searchTerm) {
+      return (
+        book.tittel.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        book.navn.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        book.sjanger.toLowerCase().includes(searchTerm.toLowerCase()) ||
+        book.aar === searchTerm
+      );
+    } else {
+      return books;
     }
+  });
 
-    return (
-      <>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <AppBar
-            position="relative"
-            style={{ height: appBarHeight, backgroundColor: "#2F5F2E" }}
-          >
-            <Toolbar style={{ display: "flex" }}>
-              <Box
-                style={{
-                  height: appBarHeight,
-                  width: appBarHeight,
-                  display: "flex",
-                  alignContent: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <img
-                  src={bookIconW}
-                  style={{
-                    height: appBarHeight,
-                    position: "relative",
-                    boxSizing: "borderbox",
-                    padding: "10px",
-                  }}
-                />
-              </Box>
-              <a href="/" style={{ color: "inherit", textDecoration: "none" }}>
-                <Typography
-                  fontSize="26px"
-                  variant="h6"
-                  noWrap
-                >
-                  ReadRater
-                </Typography>
-              </a>
-              <Box style={{ flexGrow: "1" }}></Box>
-              {user ? null : (
-                <Button
-                  variant="contained"
-                  color="success"
-                  style={{ BackgroundColor: "#2F5F2E" }}
-                  onClick={() => navigate("login")}
-                >
-                  Login
-                </Button>
-              )}
-            </Toolbar>
-          </AppBar>
-          <main>
-            <div>
-              <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
-              <button onClick={handleSearch}>Search</button>
-              {books.map((book) => (
-                <div key={book.bok_id}>
-                  <h2>{book.tittel}</h2>
-                  <p>{book.navn}</p>
-                  <p>{book.sjanger}</p>
-                  <p>{book.aar}</p>
-                </div>
-              ))}
-            </div>
-            {/* Hero unit */}
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    readService
+      .searchBooks(searchTerm)
+      .then((data) => {
+        setSearchResults(data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar
+          position="relative"
+          style={{ height: appBarHeight, backgroundColor: "#2F5F2E" }}
+        >
+          <Toolbar style={{ display: "flex" }}>
             <Box
-              sx={{
-                bgcolor: "background.paper",
-                pt: 6,
-                pb: 0,
+              style={{
+                height: appBarHeight,
+                width: appBarHeight,
+                display: "flex",
+                alignContent: "center",
+                justifyContent: "center",
               }}
             >
-              <Container
+              <img
+                src={bookIconW}
                 style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  paddingTop: 50,
+                  height: appBarHeight,
+                  position: "relative",
+                  boxSizing: "borderbox",
+                  padding: "10px",
                 }}
-              >
-                <Typography
-                  style={{ marginBottom: "30px" }}
-                  component="h1"
-                  variant="h2"
-                  align="center"
-                  color="text.primary"
-                  gutterBottom
-                >
-                  Welcome to ReadRater!
-                </Typography>
-                <Button
-                  variant="contained"
-                  color="primary"
-                  display="inline-block"
-                  width="150px"
-                  onClick={addBook}
-                >
-                  Add book
-                </Button>{" "}
-                {/*#TODO Hvis bruker er logget inn: link til addbook-side. Om nei, til login-side */}
-              </Container>
+              />
             </Box>
-            <Container sx={{ py: 4 }} maxWidth="md">
-              {/* End hero unit */}
-              <Grid
-                container
-                spacing={4}
-                style={{ marginTop: "30px", marginBottom: "0px", width: "auto" }}
+            <a href="/" style={{ color: "inherit", textDecoration: "none" }}>
+              <Typography
+                fontSize="26px"
+                variant="h6"
+                noWrap
               >
-                {books.map((book) => (
-                  <Grid item key={book.bok_id} xs={10} sm={4} md={3}>
-                    <Book
-                      title={book.tittel}
-                      author={book.navn}
-                      year={book.aar}
-                      genre={book.sjanger}
-                      picture={book.bilde}
-                      avg_rating={book.avg_verdi}
-                      bookID={book.bok_id}
-                      rateEnabled={true}
-                    ></Book>
-                    {/* #TODO Her kom å legge til infoen fra hver bok */}
-                  </Grid>
-                ))}
-              </Grid>
-            </Container>
-          </main>
-          {/* Footer */}
-          <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
-            <Typography variant="h6" align="center" gutterBottom></Typography>
-            <Typography
-              variant="subtitle1"
-              align="center"
-              color="text.secondary"
-              component="p"
+                ReadRater
+              </Typography>
+            </a>
+            <Box style={{ flexGrow: "1" }}></Box>
+            {user ? null : (
+              <Button
+                variant="contained"
+                color="success"
+                style={{ BackgroundColor: "#2F5F2E" }}
+                onClick={() => navigate("login")}
+              >
+                Login
+              </Button>
+            )}
+          </Toolbar>
+        </AppBar>
+        <main>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            label="Search for books"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.currentTarget.value)}
+          />
+          {/* Hero unit */}
+          <Box
+            sx={{
+              bgcolor: "background.paper",
+              pt: 6,
+              pb: 0,
+            }}
+          >
+            <Container
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                paddingTop: 50,
+              }}
             >
-              ReadRater: The application for rating books!
-            </Typography>
-            <Copyright />
+              <Typography
+                style={{ marginBottom: "30px" }}
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
+              >
+                Welcome to ReadRater!
+              </Typography>
+              <Button
+                variant="contained"
+                color="primary"
+                display="inline-block"
+                width="150px"
+                onClick={addBook}
+              >
+                Add book
+              </Button>{" "}
+              {/*#TODO Hvis bruker er logget inn: link til addbook-side. Om nei, til login-side */}
+            </Container>
           </Box>
-        </ThemeProvider>
-        {/* End footer */}
-      </>
-    )
-  };
-}
+          <Container sx={{ py: 4 }} maxWidth="md">
+            {/* End hero unit */}
+            <Grid
+              container
+              spacing={4}
+              style={{ marginTop: "30px", marginBottom: "0px", width: "auto" }}
+            >
+              {filterSearchedBooks.map((book) => (
+                <Grid item key={book.bok_id} xs={10} sm={4} md={3}>
+                  <Book
+                    title={book.tittel}
+                    author={book.navn}
+                    year={book.aar}
+                    genre={book.sjanger}
+                    picture={book.bilde}
+                    avg_rating={book.avg_verdi}
+                    bookID={book.bok_id}
+                    rateEnabled={true}
+                  ></Book>
+                  {/* #TODO Her kom å legge til infoen fra hver bok */}
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </main>
+        {/* Footer */}
+        <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+          <Typography variant="h6" align="center" gutterBottom></Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="text.secondary"
+            component="p"
+          >
+            ReadRater: The application for rating books!
+          </Typography>
+          <Copyright />
+        </Box>
+      </ThemeProvider>
+      {/* End footer */}
+    </>
+  )
+};
